fc84d6b50e73ccd503ea19375c1d157feccaa02a
Gautam Magar <puskarmagar687@gmail.com>
2023-08-08 03:59:32 +0545

    feat: update Teaxt area

--- frontend/src/Pages/Signin/Signin.jsx
+++ frontend/src/Pages/Signin/Signin.jsx
@@ -1,60 +1,94 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Container, Form, Row } from "react-bootstrap";
-import { Button, TextField , CircularProgress} from "@material-ui/core";
-import { Postdata } from '../../services/axios.services';
-import { useNavigate } from 'react-router-dom';
+import { Button, TextField, CircularProgress, Grid } from "@material-ui/core";
+import PasswordIcon from '@mui/icons-material/Password';
+import EmailIcon from '@mui/icons-material/Email';
+
+import { Postdata } from "../../services/axios.services";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import '../Signin/Signin.css'
 
 const Signin = () => {
-  
-  const [email ,setemail] = useState('')
-  const [password ,setpassword] =useState('')
-  const navigate = useNavigate()
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const loginsubmithandler = async(e)=>{
+  const loginsubmithandler = async (e) => {
     e.preventDefault();
 
-    const logindata ={
+    const logindata = {
       email,
-      password
+      password,
+    };
+
+    const response = await Postdata(`auth/login`, logindata);
+    console.log(response);
+
+    if (response.data.status === "success") {
+      // dispatch(auth)
+
+      navigate("/home");
     }
-
-    const response = await Postdata(`auth/login`,logindata)
-    console.log(response)
-
-    if(response.data.status === 'success'){
-
-      navigate('/home')
-    }
-
-  
-
- 
-
-
-  }
+  };
   return (
     <>
-    <div className='login'>
+      <div className="login">
+        <h1>Signin</h1>
 
-      <h1>Signin</h1>
+        <Form onSubmit={loginsubmithandler}>
+          <Form.Group>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <EmailIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Email"
+                  placeholder="Enter your email"
+                  type="email"
+                  onChange={(e) => setemail(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+          </Form.Group>
 
-      <Form onSubmit={loginsubmithandler}>
-        <Form.Group>
-          <TextField label='Email' placeholder='Enter your email'  onChange={(e)=>setemail(e.target.value)}/>
-        </Form.Group>
+          <Form.Group>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <PasswordIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Password"
+                  placeholder="Enter your password"
+           
+                  onChange={(e) => setpassword(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+          </Form.Group>
+          <Form.Group className="m-auto lgn-button">
+          <Button type="submit">Login</Button>
 
-        <Form.Group>
-          <TextField label='Password' placeholder='Enter your password'  onChange={(e)=>setpassword(e.target.value)}/>
-        </Form.Group>
+          </Form.Group>
 
-        <Button type='submit'>Login</Button>
-        
-      </Form>
+          <div className="new">
 
-    </div>
-          
-     </>
-  )
-}
+            <p><a href="/signup"> Create Account</a></p>
 
-export default Signin
+          </div>
+
+
+
+
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Signin;
--- frontend/src/Pages/Signup/Signup.jsx
+++ frontend/src/Pages/Signup/Signup.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import { Container, Form, Row } from "react-bootstrap";
-import { Button, TextField , CircularProgress} from "@material-ui/core";
+import { Button, TextField, CircularProgress, Grid } from "@material-ui/core";
 import { Postdata } from "../../services/axios.services";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import "../Signup/Signup.css";
+import PasswordIcon from "@mui/icons-material/Password";
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import MailOutlineRoundedIcon from '@mui/icons-material/MailOutlineRounded';
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 
 const Signup = () => {
   const [email, setemail] = useState("");
@@ -10,65 +15,140 @@ const Signup = () => {
   const [confirmpasswoard, setconfirmpasswoard] = useState("");
   const [name, setname] = useState("");
   const [loading, setloading] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // setloading = <CircularProgressWithLabel value={progress} />
 
-
-  const Signupsubmithandler =async(e)=>{
+  const Signupsubmithandler = async (e) => {
     e.preventDefault();
-    const Signupdata ={
+    const Signupdata = {
       email,
       password,
       confirmpasswoard,
       name,
+    };
+    console.log(Signupdata);
+
+    const response = await Postdata(`auth/register`, Signupdata);
+    console.log(response.data);
+
+    if (response.data.status) {
+      setloading(true);
+
+      console.log(response.data.message);
+      navigate("/");
     }
-    console.log(Signupdata)
-
-    
-    const response = await Postdata(`auth/register`,Signupdata)
-    console.log(response.data)
-
-    if(response.data.status){
-      setloading(true)
-
-      console.log(response.data.message)
-      navigate('/')
-
-
-    }
-   
-    
-    
-  }
+  };
   return (
     <Container>
-      <div className="SignupForm">
+      <div className="signup">
         <h1>Sign up</h1>
-        
 
         <Form onSubmit={Signupsubmithandler}>
           <Form.Group>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <PersonRoundedIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Enter Username"
+                  // placeholder="Enter your fullname"
+                  autoComplete="off"
+                  onChange={(e) => setname(e.target.value)}
+                />
+              </Grid>
+            </Grid>
+          </Form.Group>
+          {/* <Form.Group>
             <TextField label="Enter your Full name" autoComplete="off" onChange={(e)=>setname(e.target.value)} />
-          </Form.Group>
+          </Form.Group> */}
 
           <Form.Group>
-            <TextField label="Enter your email address" type="email" autoComplete="off" onChange={(e)=>setemail(e.target.value)} />
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <MailOutlineRoundedIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Enter email address"
+                  type="email"
+                  autoComplete="off"
+                  onChange={(e) => setemail(e.target.value)}
+                />
+              </Grid>
+            </Grid>
           </Form.Group>
+
+          {/* <Form.Group>
+            <TextField
+              label="Enter your email address"
+              type="email"
+              autoComplete="off"
+              onChange={(e) => setemail(e.target.value)}
+            />
+          </Form.Group> */}
 
           <Form.Group>
-            <TextField label="Enter your password" autoComplete="off" onChange={(e)=>setpassword(e.target.value)}/>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <PasswordIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Enter your password"
+                  autoComplete="off"
+                  onChange={(e) => setpassword(e.target.value)}
+                />
+              </Grid>
+            </Grid>
           </Form.Group>
+
+          {/* <Form.Group>
+            <TextField
+              label="Enter your password"
+              autoComplete="off"
+              onChange={(e) => setpassword(e.target.value)}
+            />
+          </Form.Group> */}
 
           <Form.Group>
-            <TextField label="Confirm your password" autoComplete="off" onChange={(e)=>setconfirmpasswoard(e.target.value)}/>
+            <Grid container spacing={1} alignItems="flex-end">
+              <Grid item>
+                <CheckRoundedIcon />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="input-with-icon-grid"
+                  label="Confirm your password"
+                  autoComplete="off"
+                  onChange={(e) => setconfirmpasswoard(e.target.value)}
+                />
+              </Grid>
+            </Grid>
           </Form.Group>
 
-          <Button variant="outlined" color="primary" type="submmit" disabled={loading}>
-          
-            Create Account
+          {/* <Form.Group>
+            <TextField
+              label="Confirm your password"
+              autoComplete="off"
+              onChange={(e) => setconfirmpasswoard(e.target.value)}
+            />
+          </Form.Group> */}
+
+          <Button
+            variant="outlined"
+            color="primary"
+            type="submmit"
+            disabled={loading}
+            
+          >
+           Create Account
           </Button>
-          {loading ? <CircularProgress color="red" /> : <>Create Account</> }
+          {/* {loading ? <CircularProgress color="red" /> : <>Create Account</>} */}
         </Form>
       </div>
     </Container>
