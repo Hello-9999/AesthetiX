d6c8d426cca2399a0b8e8de8fdbd441f763575ad
Gautam Magar <puskarmagar687@gmail.com>
2023-08-27 03:31:52 +0545

    feat ; Design changed

--- frontend/src/Pages/ShhipingInfo/ShippingInfo.jsx
+++ frontend/src/Pages/ShhipingInfo/ShippingInfo.jsx
@@ -6,14 +6,16 @@ import { Button, FormControl, TextField } from "@material-ui/core";
 import "./Checkout.css";
 import { useDispatch, useSelector } from "react-redux";
 import Cartslice, { Addtocart, ShippingDetail } from "../../slice/Cartslice";
+import WestIcon from "@mui/icons-material/West";
 
 const ShippingInfo = () => {
   const { ShippingAddress } = useSelector((state) => state.cart);
 
   const [email, setemail] = useState(ShippingAddress.email);
-  const [zip, setzip] = useState(ShippingAddress.zip);
+  const [postalCode, setpostalcode] = useState(ShippingAddress.postalCode);
   const [city, setcity] = useState(ShippingAddress.city);
   const [country, setcountry] = useState(ShippingAddress.country);
+  const [address, setAddress] = useState(ShippingAddress.address);
 
   const dispatch = useDispatch();
 
@@ -24,11 +26,14 @@ const ShippingInfo = () => {
 
     const shippingData = {
       email,
-      zip,
+      postalCode,
       city,
       country,
+      address,
     };
     dispatch(ShippingDetail(shippingData));
+
+    console.log(shippingData);
 
     navigate("/payment");
   };
@@ -37,25 +42,51 @@ const ShippingInfo = () => {
     <>
       <div style={{ color: "aliceblue" }} className="Shipping">
         <Container>
+          <div className="link">
+            <Link to={`/cart `}>
+              <span>
+                {" "}
+                <WestIcon />{" "}
+              </span>{" "}
+              Go Back
+            </Link>
+          </div>
           <h1>Shipping Information</h1>
           <Form className="mt-5" onSubmit={submit}>
-            <div className="Country col-4" style={{ width: "100%" }}>
-              <Form.Text className="mb-2">
-                <h6 className="mb-3">Country</h6>
-              </Form.Text>
-              <TextField
-                variant="filled"
-                placeholder="Country"
-                required
-                label="Country"
-                onChange={(e) => setcountry(e.target.value)}
-                value={country}
-                fullWidth
-              />
+            <div className="d-flex gap-5 mt-4 m-auto">
+              <div className="Country col-5">
+                <Form.Text className="mb-2">
+                  <h6 className="mb-3">Country</h6>
+                </Form.Text>
+                <TextField
+                  variant="filled"
+                  placeholder="Country"
+                  required
+                  label="Country"
+                  onChange={(e) => setcountry(e.target.value)}
+                  value={country}
+                  fullWidth
+                />
+              </div>
+
+              <div className="Address col-5">
+                <Form.Text className="mb-2">
+                  <h6 className="mb-3">Address</h6>
+                </Form.Text>
+                <TextField
+                  variant="filled"
+                  placeholder="Address"
+                  required
+                  label="Address"
+                  onChange={(e) => setAddress(e.target.value)}
+                  value={address}
+                  fullWidth
+                />
+              </div>
             </div>
 
             <div className="d-flex gap-5 mt-4 m-auto">
-              <div className="zip col-6">
+              <div className="zip col-5">
                 <Form.Text className="mb-2">
                   <h6 className="mb-3">Zip-Code</h6>
                 </Form.Text>
@@ -63,10 +94,10 @@ const ShippingInfo = () => {
                   variant="filled"
                   label="Zip-code"
                   placeholder="Zip-code"
-                  onChange={(e) => setzip(e.target.value)}
+                  onChange={(e) => setpostalcode(e.target.value)}
                   required
                   fullWidth
-                  value={zip}
+                  value={postalCode}
                 />
               </div>
 
