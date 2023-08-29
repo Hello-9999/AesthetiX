9c35b2b54a3c7e72f0831351b2d9aa5e806b8ce9
Gautam Magar <puskarmagar687@gmail.com>
2023-08-30 02:17:45 +0545

    feat ; created react toaster function

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import presistor, { store } from "./Store.jsx";
 import { PersistGate } from "redux-persist/integration/react";
-
-// import {persistor}
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ frontend/src/services/tostify.service.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+const toastconfig = {
+  position: "top-center",
+  autoClose: false,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: 0,
+  theme: "dark",
+};
+
+export const errortoast = (message) => {
+  toast.error(message, toastconfig);
+};
+export const warningtoast = (message) => {
+  toast.warning(message, toastconfig);
+};
+export const sucesstoast = (message) => {
+  toast.success(message, toastconfig);
+};
+export const infotoast = (message) => {
+  toast.info(message, toastconfig);
+};
